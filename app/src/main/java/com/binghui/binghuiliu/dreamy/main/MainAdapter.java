7f0b1ed5fcdd3f8083a7efe80d52e79595157f6d
Binghui Liu <bertieliu52@gmail.com>
2017-12-04 12:50:29 +0200

    display cells in the main fragment

---
+++ app/src/main/java/com/binghui/binghuiliu/dreamy/main/MainAdapter.java
@@ -0,0 +1,41 @@
+package com.binghui.binghuiliu.dreamy.main;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.binghui.binghuiliu.dreamy.R;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by binghuiliu on 04/12/2017.
+ */
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MainViewHolder> {
+
+    @Override
+    public MainViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.shot_cell, parent, false);
+        return new MainViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MainViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    class MainViewHolder extends RecyclerView.ViewHolder {
+
+        public MainViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
--- app/src/main/java/com/binghui/binghuiliu/dreamy/main/MainFragment.java
+++ app/src/main/java/com/binghui/binghuiliu/dreamy/main/MainFragment.java
@@ -3,6 +3,8 @@ package com.binghui.binghuiliu.dreamy.main;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,8 @@ import com.binghui.binghuiliu.dreamy.bean.Shot;
 
 import java.util.List;
 
+import butterknife.BindInt;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import timber.log.Timber;
 
@@ -20,19 +24,36 @@ import timber.log.Timber;
  */
 
 public class MainFragment extends Fragment implements ShotsContract.View {
+
+    private MainAdapter mainAdapter;
+
+    @BindView(R.id.shot_recycler_view)
+    RecyclerView shotRecyclerView;
+
+    @BindInt(R.integer.column_count)
+    int columnCount;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.main_fragment, container, false);
         ButterKnife.bind(this, view);
 
+        initRecyclerView();
+
         return view;
+    }
+
+    private void initRecyclerView() {
+        mainAdapter = new MainAdapter();
+        shotRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), columnCount));
+        shotRecyclerView.setAdapter(mainAdapter);
     }
 
     @Override
     public void load(List<Shot> shotList) {
         for (Shot shot: shotList) {
-            Timber.d("Shot in Fragment: %s %s, image: %s", shot.id(), shot.title(), shot.images().normal());
+
         }
     }
 }
